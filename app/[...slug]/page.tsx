844fc47c896d9ee4523e56e53b46c803520e6951
BNS Works Contracts Private Limited <info@bnsworkscontracts.com>
2026-09-02 02:27:12 +0530

    Configure GitHub Pages deployment

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,5 +1,16 @@
 import SiteApp from '../../src/main'
 
+export const dynamicParams = false
+
+export function generateStaticParams() {
+  return [
+    { slug: ['about'] },
+    { slug: ['services'] },
+    { slug: ['capabilities'] },
+    { slug: ['contact'] },
+  ]
+}
+
 export default function SitePage() {
   return <SiteApp />
 }
